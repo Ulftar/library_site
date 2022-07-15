109e42b6f04f6b734086254de7173e62f600d70c
Ulftar <alexandr.kolodnitskiy@gmail.com>
2022-07-15 15:38:33 +0700

    Create template for book list

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre
 
 def index(request):
@@ -22,3 +22,7 @@ def index(request):
         	'num_books':num_books,'num_instances':num_instances,
     		'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
+
+class BookListView(generic.ListView):
+    model = Book
+    
